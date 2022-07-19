0c37861293cc9c92ccb1dc6ca87bd7d3ac5202f3
ziinup <wlsgmlxo@gmail.com>
2022-07-19 17:08:34 +0900

    until class 7.2

---
+++ syntax/Variable2.js
@@ -0,0 +1,2 @@
+var letter = "Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.";
+console.log(letter);
