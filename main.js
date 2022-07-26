fe7db1a78194b0efdb74e4647c2985073c3c3425
ziinup <wlsgmlxo@gmail.com>
2022-07-26 17:35:21 +0900

    new file:   data/SYNC 	new file:   data/TEST 	modified:   main.js

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
+function templateHTML(title, list, body){
+  return `<!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    <a href="/create">CREATE</a>
+    ${body}
+    <hr>
+    <p>node.js 32</p>
+  </body>
+  </html>`;
+};
+
+function listing(target){
+  var i = 0;
+  var list = '<ol>';
+  while(i < target.length){
+    var list = list + `<li><a href="/?id=${target[i]}">${target[i]}</a></li>`;
+   i = i+1;
+  }
+  var list = list + '</ol>';
+  return list;
+};
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -10,72 +39,49 @@ var app = http.createServer(function(request,response){
     var title = queryData.id;
 
     if(pathname === '/'){
-      if(queryData.id === undefined){  
-        fs.readdir('./data', function(error, filelist){
-          var i = 0;
-          var list = '<ol>';
-          while(i < filelist.length){
-            var list = list + `<li><a href="?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i+1;
-          }
-          var list = list + '</ol>';
-        
+      if(queryData.id === undefined){
         var title = 'Welcome';
         var description = 'Hello, Node.js';
-
-        var template = `<!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            ${list}
-          </ol>
-          <h2>${title}</h2>
-          <p style="margin-top:45px;">
-            ${description}
-          </p>
-          <hr>
-          <p>node.js 23</p>
-        </body>
-        </html>`;
-        response.writeHead(200);
-        response.end(template);
-      });
+        
+        fs.readdir('./data', function(error, filelist){
+          var list = listing(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2><p style="margin-top:45px;">${description}</p>`);
+          response.writeHead(200);
+          response.end(template);
+        });
         } else {
-          fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
-            var template = `<!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              <ol>
-                <li><a href="?id=html">HTML</a></li>
-                <li><a href="?id=css">CSS</a></li>
-                <li><a href="?id=javascript">JavaScript</a></li>
-              </ol>
-              <h2>${title}</h2>
-              <p style="margin-top:45px;">
-                ${description}
-              </p>
-              <hr>
-              <p>node.js 23</p>
-            </body>
-            </html>`;
-            response.writeHead(200);
-            response.end(template);
+          fs.readdir('./data', function(error, filelist){
+            var list = listing(filelist);
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
+              var template = templateHTML(title, list, `<h2>${title}</h2><p style="margin-top:45px;">${description}</p>`);
+              response.writeHead(200);
+              response.end(template);
+            });
           });
         }
+      } else if(pathname === '/create'){
+        fs.readdir('./data', function(error, filelist){
+          var list = listing(filelist);
+          var template = templateHTML(title, list, `<form action="http://localhost:3000/create_process" method="post"><div><label for="tit">Title<label><input id="tit" name="tit" type="text" /></div><div><label for="conts">Contents</label><textarea id="conts" name="conts"></textarea></div><button>Insert</button></form>`);
+          response.writeHead(200);
+          response.end(template);
+        });
+      }else if(pathname === '/create_process'){
+        var body = '';
+        request.on('data', function(data){
+          body = body + data;
+        });
+        request.on('end', function(){
+          var post = qs.parse(body);
+          var title = post.tit;
+          var contents = post.conts;
+          console.log(post);
+        });
+        response.writeHead(200);
+        response.end('Success');
       } else {
         response.writeHead(404);
         response.end('Not found');
       }
-
 });
-app.listen(3000);  
+app.listen(3000);
